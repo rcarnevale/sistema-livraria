e6c3312e6a3b0b4a86a19484937d1263081174d8
Rodrigo Carnevale <53884691+rcarnevale@users.noreply.github.com>
2019-10-31 17:41:21 -0300

    Build 5
    Build semi-funcional. 
    Ainda com erros no map das tabelas. Possivelmente resultante da resposta dos servidores que hospedam as requisições.

---
+++ build/precache-manifest.46a022f415109b4198f06e69dd208598.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1745d3985557386226e7ae04b1ad918",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ff307d9b140b1d81234",
+    "url": "/static/css/main.dbc6b2c8.chunk.css"
+  },
+  {
+    "revision": "97eea597f3c2608456ac",
+    "url": "/static/js/2.1fd48443.chunk.js"
+  },
+  {
+    "revision": "0ff307d9b140b1d81234",
+    "url": "/static/js/main.22885514.chunk.js"
+  },
+  {
+    "revision": "5a1fd762cac22b23bfc6",
+    "url": "/static/js/runtime-main.40ebad5a.js"
+  }
+]);
